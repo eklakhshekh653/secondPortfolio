b3c55fd3d70f62fd9c8002c5b39ecf59e6613bd5
eklak <eklakhshekh653@gmail.com>
2024-11-17 13:32:01 +0530

    first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,52 @@
+import React, { useEffect, useRef } from "react";
+import pdf from "./resume.pdf";
+import ak from "./data/my-img.json";
+import Typed from "typed.js";
+
+const Home = () => {
+  const typedRef = useRef(null);
+  useEffect(() => {
+    const options = {
+      strings: [
+        "Welcome to my profile",
+        "My Name is Shekh Eklakh",
+        "I'm full stack developer",
+        "Backend Developer (Spring Boot)",
+      ],
+      typeSpeed: 50,
+      backSpeed: 50,
+      loop: true,
+    };
+
+    const typed = new Typed(typedRef.current, options);
+
+    return () => {
+      typed.destroy();
+    };
+  }, []);
+
+  return (
+    <>
+      <div className="container home" id="home">
+        <div className="left" data-aos="fade-up-right" data-aos-duration="1000">
+          <h1 ref={typedRef}></h1>
+
+          <a
+            href={pdf}
+            download="resume.pdf"
+            className="btn btn-outline-warning my-3"
+          >
+            Download Resume
+          </a>
+        </div>
+        <div className="right">
+          <div className="img" data-aos="fade-up-left" data-aos-duration="1000">
+            <img src={`/assets/${ak.imgSrc}`} alt="Eklakh" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
